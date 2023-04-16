7e661ddea112438a8628af79a51f0326707b48e6
Owen T Penman <openman@banyanlabs.io>
2023-04-15 22:39:36 -0700

    updated main links in footer

--- src/components/FooterDisplay.tsx
+++ src/components/FooterDisplay.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, useState, useEffect } from "react";
 import { sortedAPIdata, APIdata } from "./Articles";
 import { Link } from "react-router-dom";
 
@@ -10,15 +10,24 @@ type Props = {
 }
 
 export const FooterDisplay:FC<Props> = ({topFooterFeedState, footerDisplayState,keyPlacement, bottomPlacement}) => {
+    const [topLink, setTopLink] = useState<APIdata|null>(null)
     const topKey = Object.keys(topFooterFeedState).slice()[keyPlacement];
     const bottomDisplay = Object.entries(footerDisplayState).slice(bottomPlacement-4, bottomPlacement);
     const encodedData = (data: APIdata): string =>{
         const newEncoded = encodeURIComponent(JSON.stringify(data));
         return newEncoded;
     }
+    useEffect(()=>{
+        if(topFooterFeedState && topKey){
+        setTopLink(topFooterFeedState[topKey][0])
+        }
+        return()=>{
+            setTopLink(null);
+        }
+    },[topKey, topFooterFeedState])
     return (
       <>
-        <h3 className="text-sm font-semibold leading-6 text-white cursor-pointer"><Link to={`/article/${encodedData(topFooterFeedState[topKey][0])}`}>{topKey}</Link></h3>
+        <h3 className="text-sm font-semibold leading-6 text-white"><Link to={ topLink ? `/article/${encodedData(topLink)}` : '#'}>{topKey}</Link></h3>
         <ul role="list" className="mt-6 space-y-4">
           {bottomDisplay.map(
             (item: [key: string, value: APIdata[]], index: number) => (
